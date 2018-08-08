da1845eed5f1fd40d5ab15cf7bf54df87a98bddf
Anthony Scopatz <scopatz@gmail.com>
2018-08-08 16:20:13 -0400

    remove mac specific stuff and add license

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,6 @@
 set -ex
 
 UNAME="$(uname)"
-if [ "${UNAME}" == "Darwin" ]; then
-  # for Mac OSX
-  # This is here to prevent issuses with no finding type_traits headers
-  # though this is done in toolchain, so it seems odd that it is needed.
-  export MACOSX_VERSION_MIN="10.9"
-  export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LINKFLAGS="${LDFLAGS}"
-fi
 
 ./configure --prefix="${PREFIX}" || { cat config.log; exit 1; }
 make
